eeaa099719163f219d2fb5c349c1ee48c99d75a1
sp55 <1172242456@qq.com>
2016-08-12 09:22:14 +0800

    iOS中求出label中文字的行数和每一行的内容
    iOS中求出label中文字的行数和每一行的内容

---
+++ CountNumOfLabel_Demo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  CountNumOfLabel_Demo
+//
+//  Created by admin on 16/8/12.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreText/CoreText.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CountNumOfLabel_Demo/ViewController.m
@@ -0,0 +1,66 @@
+//
+//  ViewController.m
+//  CountNumOfLabel_Demo
+//
+//  Created by admin on 16/8/12.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+//http://www.jianshu.com/p/33b93b39311a
+//iOS中求出label中文字的行数和每一行的内容
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    UILabel *testLabel=[[UILabel alloc] initWithFrame:CGRectMake(10, 100, self.view.frame.size.width-20, 200)];
+    testLabel.text =  @"披绣闼，俯雕甍，山原旷其盈视，川泽纡其骇瞩。闾阎扑地，钟鸣鼎食之家；舸舰迷津，青雀黄龙之舳。云销雨霁，彩彻区明。落霞与孤鹜齐飞，秋水共长天一色。渔舟唱晚，响穷彭蠡之滨，雁阵惊寒，声断衡阳之浦。";
+    testLabel.backgroundColor = [UIColor redColor];
+    testLabel.textColor = [UIColor whiteColor];
+    testLabel.numberOfLines = 0;
+    [self.view addSubview:testLabel];
+    
+
+    NSArray *testArr=[self getLinesArrayOfStringInLabel:testLabel];
+    NSLog(@"======%ld",testArr.count);
+
+}
+
+- (NSArray *)getLinesArrayOfStringInLabel:(UILabel *)label{
+    NSString *text = [label text];
+    UIFont *font = [label font];
+    CGRect rect = [label frame];
+    
+    CTFontRef myFont = CTFontCreateWithName(( CFStringRef)([font fontName]), [font pointSize], NULL);
+    NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:text];
+    [attStr addAttribute:(NSString *)kCTFontAttributeName value:(__bridge  id)myFont range:NSMakeRange(0, attStr.length)];
+    CFRelease(myFont);
+    CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString(( CFAttributedStringRef)attStr);
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, CGRectMake(0,0,rect.size.width,100000));
+    CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, 0), path, NULL);
+    NSArray *lines = ( NSArray *)CTFrameGetLines(frame);
+    NSMutableArray *linesArray = [[NSMutableArray alloc]init];
+    for (id line in lines) {
+        CTLineRef lineRef = (__bridge  CTLineRef )line;
+        CFRange lineRange = CTLineGetStringRange(lineRef);
+        NSRange range = NSMakeRange(lineRange.location, lineRange.length);
+        NSString *lineString = [text substringWithRange:range];
+        CFAttributedStringSetAttribute((CFMutableAttributedStringRef)attStr, lineRange, kCTKernAttributeName, (CFTypeRef)([NSNumber numberWithFloat:0.0]));
+        CFAttributedStringSetAttribute((CFMutableAttributedStringRef)attStr, lineRange, kCTKernAttributeName, (CFTypeRef)([NSNumber numberWithInt:0.0]));
+        NSLog(@"lineString--->%@",lineString);
+        [linesArray addObject:lineString];
+    }
+    
+    CGPathRelease(path);
+    CFRelease( frame );
+    CFRelease(frameSetter);
+    return (NSArray *)linesArray;
+}@end
